b9e715c7c2c9b4f68bfd47ee35b793785ee8494a
Gwenael YAP <gyap@ymagyn.com>
2013-11-20 09:56:04 +0100

    Correction du plugin android

--- src/android/EmailComposer.java
+++ src/android/EmailComposer.java
@@ -14,115 +14,127 @@ import android.text.Html;
 
 public class EmailComposer extends CordovaPlugin {
 
-        @Override
-        public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+	@Override
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 
-                if ("showEmailComposer".equals(action)) {
-                                JSONObject parameters = args.getJSONObject(0);
-                                if (parameters != null) {
-                                        sendEmail(parameters);
-                                }
-                        
-                        callbackContext.success();
-                        return true;
-                }
-                return false;  // Returning false results in a "MethodNotFound" error.
-        }
+		if ("showEmailComposer".equals(action)) {
+			JSONObject parameters = args.getJSONObject(0);
+			if (parameters != null) {
+				sendEmail(parameters);
+			}
 
-        private void sendEmail(JSONObject parameters) {
-                
-                final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND_MULTIPLE);               
+			callbackContext.success();
+			return true;
+		}
+		return false;  // Returning false results in a "MethodNotFound" error.
+	}
 
-                boolean isHTML = false;
-                try {
-                        isHTML = parameters.getBoolean("bIsHTML");
-                } catch (Exception e) {
-                		isHTML = false;
-                }
+	private void sendEmail(JSONObject parameters) {
 
-                if (isHTML) {
-                        emailIntent.setType("text/html");
-                } else {
-                        emailIntent.setType("text/plain");
-                }
+		final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND_MULTIPLE);              
 
-                // setting subject
-                try {
-                        String subject = parameters.getString("subject");
-                        if (subject != null && subject.length() > 0) {
-                                emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, subject);
-                        }
-                } catch (Exception e) {
-                        LOG.e("EmailComposer", "Error handling subject param: " + e.toString());
-                }
+		boolean isHTML = false;
+		if(parameters.has("bIsHTML")){
+			try {
+				isHTML = parameters.getBoolean("bIsHTML");
+			} catch (Exception e) {
+				isHTML = false;
+			}
+		}
+		if (isHTML) {
+			emailIntent.setType("text/html");
+		} else {
+			emailIntent.setType("text/plain");
+		}
 
-                // setting body
-                try {
-                        String body = parameters.getString("body");
-                        if (body != null && body.length() > 0) {
-                        	ArrayList<String> b = new ArrayList<String>();
-                        	b.add(body);
-                                if (isHTML) {
-                                        emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, Html.fromHtml(body));
-                                } else {
-                                        emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, b);
-                                }
-                        }
-                } catch (Exception e) {
-                        LOG.e("EmailComposer", "Error handling body param: " + e.toString());
-                }
 
-                // setting TO recipients
-                try {
-                        String toRecipients = parameters.getString("toRecipients");
-                        if (toRecipients != null && toRecipients.length() > 0) {
-                        	 	ArrayList<String> to = new ArrayList<String>();
-                        	 	String[] toTemp = toRecipients.split(";");
-                        	 	for(int i = 0; i<toTemp.length; i++){
-                        	 		to.add(toTemp[i]);
-                        	 	}
-                                emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, to);
-                        }
-                } catch (Exception e) {
-                        LOG.e("EmailComposer", "Error handling toRecipients param: " + e.toString());
-                }
+		// setting subject
+		if(parameters.has("subject")){
+			try {
+				String subject = parameters.getString("subject");
+				if (subject != null && subject.length() > 0) {
+					emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, subject);
+				}
+			} catch (Exception e) {
+				LOG.e("EmailComposer", "Error handling subject param: " + e.toString());
+			}
+		}
 
-                // setting CC recipients
-                try {
-                	String ccRecipients = parameters.getString("ccRecipients");
-                        if (ccRecipients != null && ccRecipients.length() > 0) {
-                        		ArrayList<String> cc = new ArrayList<String>();
-                        	 	String[] ccTemp = ccRecipients.split(";");
-                        	 	for(int i = 0; i<ccTemp.length; i++){
-                        	 		cc.add(ccTemp[i]);
-                        	 	}
-                                emailIntent.putExtra(android.content.Intent.EXTRA_CC, cc);
-                        }
-                } catch (Exception e) {
-                        LOG.e("EmailComposer", "Error handling ccRecipients param: " + e.toString());
-                }
+		// setting body
 
-                // setting BCC recipients
-                try {
-                	String bccRecipients = parameters.getString("bccRecipients");
-                        if (bccRecipients != null && bccRecipients.length() > 0) {
-                                ArrayList<String> bcc = new ArrayList<String>();
-                        	 	String[] bccTemp = bccRecipients.split(";");
-                        	 	for(int i = 0; i<bccTemp.length; i++){
-                        	 		bcc.add(bccTemp[i]);
-                        	 	}
-                                emailIntent.putExtra(android.content.Intent.EXTRA_BCC, bcc);
-                        }
-                } catch (Exception e) {
-                        LOG.e("EmailComposer", "Error handling bccRecipients param: " + e.toString());
-                }
-                
-                 this.cordova.getActivity().startActivity(emailIntent);
-        }
-        @Override
-        public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-                super.onActivityResult(requestCode, resultCode, intent);
-                LOG.e("EmailComposer", "ResultCode: " + resultCode);
-                // IT DOESN'T SEEM TO HANDLE RESULT CODES
-        }
+		if(parameters.has("body")){
+			String body;
+			try {
+				body = parameters.getString("body");
+				if (body != null && body.length() > 0) {
+					if (isHTML) {
+						emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, Html.fromHtml(body));
+					} else {
+						emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, body);
+					}
+				}
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+
+		// setting TO recipients
+		if(parameters.has("toRecipients")){
+			String toRecipients;
+			try {
+				toRecipients = parameters.getString("toRecipients");
+				if (toRecipients != null && toRecipients.length() > 0) {
+					String [] to = toRecipients.split(";");
+					emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, to);
+				}
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+
+		// setting CC recipients
+		if(parameters.has("ccRecipients")){
+			String ccRecipients;
+			try {
+				ccRecipients = parameters.getString("ccRecipients");
+				if (ccRecipients != null && ccRecipients.length() > 0) {
+					String [] cc = ccRecipients.split(";");
+					emailIntent.putExtra(android.content.Intent.EXTRA_CC, cc);
+				}
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+		// setting BCC recipients
+		if(parameters.has("bccRecipients")){
+			String bccRecipients;
+			try {
+				bccRecipients = parameters.getString("bccRecipients");
+				if (bccRecipients != null && bccRecipients.length() > 0) {
+					String [] bcc = bccRecipients.split(";");
+					emailIntent.putExtra(android.content.Intent.EXTRA_BCC, bcc);
+				}
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+		this.cordova.getActivity().startActivity(emailIntent);
+	}
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		super.onActivityResult(requestCode, resultCode, intent);
+		LOG.e("EmailComposer", "ResultCode: " + resultCode);
+		// IT DOESN'T SEEM TO HANDLE RESULT CODES
+	}
 }
